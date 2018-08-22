84300de1d0ac0f967315d38d6a4cb38d36f44536
baitieyi <baitieyi@163.com>
2018-08-22 16:51:32 +0800

    index

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,12 +3,30 @@ module.exports = {
   description: 'Coming soon',
   base: '/',
   themeConfig: {
+    displayAllHeaders: true,
+    // sidebar: 'auto',
     sidebar: [
       '/',
-      '/docs/aclius',
-      '/docs/programmingwithcpp',
-      '/docs/helloworldkernel',
-      '/docs/rese0x0'
+      {
+        title: 'org',
+        children: [
+          '/docs/aclius',
+          '/docs/backhome',
+          '/docs/backhomeparty',
+          '/docs/backhomekernel',
+          '/docs/yuki',
+          '/docs/miu',
+          '/docs/onenumber'
+        ]
+      },
+      {
+        title: 'tech doc',
+        children: [
+          '/docs/programmingwithcpp',
+          '/docs/helloworldkernel',
+          '/docs/rese0x0'
+        ]
+      }
     ]
   }
 }
